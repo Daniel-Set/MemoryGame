06e54bef636bf3c470d451f3e2b046e931bff58b
daniel <hetman.set@gmail.com>
2022-07-18 23:53:01 +0200

    Unit Tests added

---
+++ memory_game/src/test/java/pl/setlikD/Motorola/FileLoaderTest.java
@@ -0,0 +1,20 @@
+package pl.setlikD.Motorola;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileLoaderTest {
+
+    @Test
+    void loadWordsList() {
+    }
+
+    @Test
+    void saveRecord() {
+    }
+
+    @Test
+    void printTop10() {
+    }
+}
---
+++ memory_game/src/test/java/pl/setlikD/Motorola/PlayerTest.java
@@ -0,0 +1,16 @@
+package pl.setlikD.Motorola;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void printCongrats() {
+    }
+
+    @Test
+    void top10Printer() {
+    }
+}
